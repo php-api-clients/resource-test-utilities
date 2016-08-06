61f22635ae9c25ca580adb6b08e1462ea366d29c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-08-06 23:37:57 +0200

    WIP incompatible support

--- src/AbstractResourceTest.php
+++ src/AbstractResourceTest.php
@@ -9,6 +9,7 @@ use Generator;
 use phpDocumentor\Reflection\DocBlock;
 use phpDocumentor\Reflection\DocBlockFactory;
 use ReflectionClass;
+use ReflectionProperty;
 
 abstract class AbstractResourceTest extends TestCase
 {
@@ -17,18 +18,35 @@ abstract class AbstractResourceTest extends TestCase
 
     public function provideProperties(): Generator
     {
-        $jsonTemplate = [];
+        yield from $this->providePropertiesGenerator('compatible');
+    }
+
+    public function providePropertiesIncompatible(): Generator
+    {
+        yield from $this->providePropertiesGenerator('incompatible');
+    }
+
+    public function providePropertiesGenerator(string $typeMethod): Generator
+    {
+        foreach (Types::types() as $t) {
+            var_export($t);
+        }
         $class = new ReflectionClass($this->getClass());
+
+        $jsonTemplate = [];
         foreach ($class->getProperties() as $property) {
             $jsonTemplate[$property->getName()] = '';
         }
+
         foreach ($class->getProperties() as $property) {
             $method = Inflector::camelize($property->getName());
             $docBlock = $this->getDocBlock($property->getDocComment());
+
             $varTag = $docBlock->getTagsByName('var');
             if (count($varTag) !== 1) {
                 continue;
             }
+
             $varTag = $varTag[0];
             if ($varTag->getType() === '') {
                 continue;
@@ -38,10 +56,18 @@ abstract class AbstractResourceTest extends TestCase
                 continue;
             }
 
-            $json = $jsonTemplate;
-            foreach (Types::get($varTag->getType())->generate(25) as $value) {
+            $type = Types::get($varTag->getType());
+            yield from $this->generateTypeValues($type, $property, $typeMethod, $jsonTemplate);
+        }
+    }
+
+    protected function generateTypeValues(Type $type, ReflectionProperty $property, string $method, array $jsonTemplate): Generator
+    {
+        $json = $jsonTemplate;
+        foreach ($type->$method as $typeClass) {
+            $methodType = Types::get(constant($typeClass . '::SCALAR'));
+            foreach ($methodType->generate(25) as $value) {
                 $json[$property->getName()] = $value;
-                $type = Types::has($varTag->getType());
                 yield [
                     $property->getName(), // Name of the property to assign data to
                     $method,              // Method to call verifying that data
@@ -83,6 +109,25 @@ abstract class AbstractResourceTest extends TestCase
         );
         $this->assertSame($value, $resource->{$method}());
         $this->assertInternalType($type->scalar(), $resource->{$method}());
+    }
+
+    /**
+     * @dataProvider providePropertiesIncompatible
+     */
+    public function testPropertiesIncompatible(string $property, string $method, Type $type, array $json, mixed $value)
+    {
+        var_export([$property, $method, $type, $json, $value]);
+        $class = $this->getClass();
+        $resource = $this->hydrate(
+            str_replace(
+                $this->getNamespace(),
+                $this->getNamespace() . '\\Async',
+                $class
+            ),
+            $json,
+            'Async'
+        );
+        $this->assertSame($value, $resource->{$method}());
     }
 
     public function testInterface()
