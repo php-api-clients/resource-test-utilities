50a4394d3df8462b5325a1852d49b12f31c880dd
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-08-06 23:14:46 +0200

    test generate count

--- tests/Type/AbstractTypeTest.php
+++ tests/Type/AbstractTypeTest.php
@@ -13,8 +13,41 @@ abstract class AbstractTypeTest extends \PHPUnit_Framework_TestCase
         $internalType = $this->getType();
         $class = $this->getClass();
         $type = new $class();
-        foreach ($type->generate(100000) as $row) {
+        foreach ($type->generate(25000) as $row) {
             $this->assertInternalType($internalType, $row);
         }
+    }
+
+    public function provideGenerateCount()
+    {
+        yield [1];
+        yield [13];
+        yield [123];
+        yield [1000];
+    }
+
+    /**
+     * @dataProvider provideGenerateCount
+     */
+    public function testGenerateCount(int $count)
+    {
+        $class = $this->getClass();
+        $type = new $class();
+        $rows = 0;
+        foreach ($type->generate($count) as $row) {
+            $rows++;
+        }
+        $this->assertSame($count, $rows);
+    }
+
+    public function testGenerateDefaultCount()
+    {
+        $class = $this->getClass();
+        $type = new $class();
+        $rows = 0;
+        foreach ($type->generate() as $row) {
+            $rows++;
+        }
+        $this->assertSame(100, $rows);
     }
 }
