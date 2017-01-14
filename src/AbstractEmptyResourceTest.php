731744654290d7335f7b5297cb9cd7bf896cc166
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-01-14 19:25:16 +0100

    AbstractEmptyResourceTest

---
+++ src/AbstractEmptyResourceTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace ApiClients\Tools\ResourceTestUtilities;
+
+use phpDocumentor\Reflection\DocBlock;
+use ReflectionClass;
+use Throwable;
+use TypeError;
+
+abstract class AbstractEmptyResourceTest extends TestCase
+{
+    abstract public function getSyncAsync(): string;
+    abstract public function getClass(): string;
+
+    public function provideProperties(): array
+    {
+        $yield = [];
+        $class = new ReflectionClass($this->getClass());
+
+        foreach ($class->getMethods() as $method) {
+            $yield[] = [$method->getName()];
+        }
+
+        return [[$yield]];
+    }
+
+    /**
+     * @dataProvider provideProperties
+     */
+    public function testProperties($args)
+    {
+        $class = $this->getClass();
+        $resource = new $class();
+
+        foreach ($args as $arg) {
+            list ($method) = $arg;
+
+            try {
+                $resource->{$method}();
+            } catch (TypeError $e) {
+                self::assertTrue(true);
+                continue;
+            } catch (Throwable $t) {
+                self::fail('Should have thrown a TypeError instead of a ' . get_class($t));
+            }
+        }
+    }
+}
